634232460d84c6654578ffd0737e259f0d7e4032
LouisChang <louis60406@gmail.com>
2017-11-22 22:15:06 +0800

    C# Practice

---
+++ C# Practice/Practice001/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice001
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1=Convert.ToInt32(box1.Text);
+            int num2=Convert.ToInt32(box2.Text);
+            if (num1 > num2)
+            {
+                label1.Text = "BIG";
+            }
+            else
+            {
+                label1.Text="SMALL";
+            }
+        }
+    }
+}

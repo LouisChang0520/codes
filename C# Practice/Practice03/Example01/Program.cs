f04b2aef513df11fec57a48af0eafabcbfaf7ae6
LouisChang <louis60406@gmail.com>
2018-02-14 17:45:32 +0800

    20180214 C#陣列＋迴圈（九九乘法表）

---
+++ C# Practice/Practice03/Example01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] main = new int[10];
+            for (int numbers = 1; numbers <= main.Length; numbers++)
+            {
+                Console.WriteLine("九九乘法表{0}:", numbers);
+                for (int num0 = 1; num0 <= main.Length; num0++)
+                {
+                    Console.WriteLine(numbers + " X " + num0 + "=" + (numbers * num0), numbers);
+                }
+            }
+        }
+    }
+}
